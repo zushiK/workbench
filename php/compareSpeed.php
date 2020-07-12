7f0e2707c2da20bb0bfdbecbced70b71b0d63a35
zushi <zushi0905@gmail.com>
2020-07-13 00:26:46 +0900

    inarray_isset : true配列を作ってissetが一番早い

--- php/compareSpeed.php
+++ php/compareSpeed.php
@@ -1,24 +1,45 @@
 <?php 
 // https://thk.kanzae.net/net/itc/t2379/
 // 計測開始
-$obj = new CompareSpeed();
-$time_start = microtime(true);
+$loop_count = 1000;
+inarray_isset();
 
-$time = microtime(true) - $time_start;
-echo $time . "秒";
+function inarray_isset(){
+    global $loop_count;
+    $array = ["apple", "orange", 'melon', "banana"];
 
-
-class CompareSpeed
-{
-    public function inArrayFnc(){
-        $array = [
-            'apple',
-            'orange',
-            'melon',
-            'banana'
-        ];
+    $st_a = microtime(true);
+    for ($i=0; $i < $loop_count; $i++) { 
+        if(in_array( 'orange', $array ))
+        {
+            echo "true";
+        }
     }
+    $end_a = microtime(true);
+
+    $st_b = microtime(true);
+    for ($i=0; $i < $loop_count; $i++) { 
+        for ($j=0; $j < count($array) ; $j++) { 
+            $new_array[$array[$j]] = true;
+        }
+        if (isset( $array['orange'])){
+            echo "true";
+        }
+    }
+    $end_b = microtime(true);
+
+    $st_c = microtime(true);
+    for ($i=0; $i < $loop_count; $i++) { 
+        for ($j=0; $j < count($array) ; $j++) { 
+            if($array[$i] === 'orange'){
+                echo "true";
+            }
+        }
+    }
+    $end_c = microtime(true);
+
+    echo "\n";
+    echo '処理aの処理時間:'.($end_a - $st_a)."秒<br>\n";
+    echo '処理bの処理時間:'.($end_b - $st_b)."秒<br>\n";
+    echo '処理cの処理時間:'.($end_c - $st_c)."秒<br>\n";
 }
-
-
-?>
